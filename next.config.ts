3f7a58e35ddace05cc8e4d447dcc43b672a022b9
Angel Lakra <angel.lakra@chainlabs.in>
2025-12-12 12:01:37 +0530

    chore: update Next.js configuration and TypeScript settings
    - Remove custom webpack configuration from next.config.ts for simplicity
    - Update TypeScript target from ES2017 to ES2020 in tsconfig.json
    - Adjust build script in package.json to specify webpack explicitly

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // Webpack configuration for production builds
-  webpack: (config) => {
-    config.externals.push(
-      "pino-pretty",
-      "lokijs",
-      "encoding",
-      "@react-native-async-storage/async-storage"
-    );
-    return config;
-  },
 };
 
 export default nextConfig;
